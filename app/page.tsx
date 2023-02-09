dfea2590d18c33b1f8c8626fa34ea7a30fb5b5ac
Jian Liao <jianliao@adobe.com>
2023-02-09 08:34:32 -0800

    Reproduce the nextjs 13 bundler bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,43 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import { SpButton } from "nextjs13-bundle-bug";
+import { SpTheme } from "@/theme";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <SpTheme>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <SpButton>
+              <code className={styles.code}>app/page.tsx</code>
+            </SpButton>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
         </div>
-      </div>
-
+      </SpTheme>
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -87,5 +94,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
